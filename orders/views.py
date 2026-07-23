212082bbb77ed7cef01860860ddeadd8e390f2de
karybekovarajana116-arch <karybekovarajana116@gmail.com>
2026-07-23 17:10:10 +0600

    glow

--- orders/views.py
+++ orders/views.py
@@ -1,31 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Order
 
-
+@login_required
 def checkout(request):
-
     if request.method == "POST":
-
         Order.objects.create(
-
             user=request.user,
-
             name=request.POST['name'],
-
             email=request.POST['email'],
-
             address=request.POST['address']
-
         )
 
+        return render(request, 'orders/success.html')
 
-        return render(
-            request,
-            'orders/success.html'
-        )
-
-
-    return render(
-        request,
-        'orders/checkout.html'
-    )
+    return render(request, 'orders/checkout.html')
